8b73df768198ae098c7a8630500c94dd721816b8
Dan <Dan@Dinkydetails.com>
2013-05-15 23:17:33 -0400

    A lot more Red x's going on than the videos !! Something isnt clicking

--- JavaII_Week2/gen/com/dannis/javaii_week1/R.java
+++ JavaII_Week2/gen/com/dannis/javaii_week1/R.java
@@ -20,8 +20,9 @@ public final class R {
         public static final int buttonSearch=0x7f070005;
         public static final int editText1=0x7f070002;
         public static final int editText2=0x7f07000b;
+        public static final int fragment1=0x7f07000d;
         public static final int imageView1=0x7f070000;
-        public static final int menu_settings=0x7f07000d;
+        public static final int menu_settings=0x7f07000e;
         public static final int textView1=0x7f070001;
         public static final int textView2=0x7f07000a;
         public static final int textView3=0x7f070004;
